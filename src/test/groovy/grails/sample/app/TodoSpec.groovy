f10f37a4456c3fed4050bf9e9aa0678b4fe18583
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-06-11 16:48:29 +0900

    add todo domain test

--- src/test/groovy/grails/sample/app/TodoSpec.groovy
+++ src/test/groovy/grails/sample/app/TodoSpec.groovy
@@ -15,8 +15,50 @@ class TodoSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "date and description are not blank"() {
+        when:
+        def test = new Todo(date: '2016/05/05', description: 'test')
+
+        then:
+        test.validate()
+    }
+
+    void "date and descriptopn are blank"() {
+        setup:
+        def test = new Todo(date: date, description: description)
+
+        expect:
+        !test.validate()
+
+        where:
+        date | description
+        ''   | ''
+        null | null
+    }
+
+    void "date is blank"() {
+        setup:
+        def test = new Todo(date: date, description: description)
+
+        expect:
+        !test.validate()
+
+        where:
+        date | description
+        ''   | 'test'
+        null | 'test'
+    }
+
+    void "description is blank"() {
+        setup:
+        def test = new Todo(date: date, description: description)
+
+        expect:
+        !test.validate()
+
+        where:
+        date       | description
+        '2016/1/1' | ''
+        '2016/1/1' | null
     }
 }
